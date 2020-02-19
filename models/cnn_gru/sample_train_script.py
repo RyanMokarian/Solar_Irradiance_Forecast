86de3f53871079055a7cd5874655228b49d29368
azfarkhoja305 <azfarkhoja305@hotmail.com>
2020-02-19 13:55:15 +0000

    Fix missing import

--- models/cnn_gru/sample_train_script.py
+++ models/cnn_gru/sample_train_script.py
@@ -7,6 +7,7 @@ import tensorflow as tf
 from tensorflow import keras,data
 from tensorflow.keras import layers,models,activations
 from model_utils import *
+from cnn_gru import *
 
 
 model = Full_Model(CNN,Encoder,Decoder,BahdanauAttention)
